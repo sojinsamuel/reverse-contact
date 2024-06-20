6feab98a0fdee2cfb988a8a7d2ad8ed018d88c1b
sojinsamuel <sojinsamuel2001@gmail.com>
2024-06-21 03:05:12 +0530

    change env name

--- aws/dynamodb/DynamoDBClient.ts
+++ aws/dynamodb/DynamoDBClient.ts
@@ -11,7 +11,7 @@ import {
 const client = new DynamoDBClient({
   region: "us-east-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
